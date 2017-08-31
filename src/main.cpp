ba59a20db0c996619e2b6a8ffc1e76c4d88656d0
Londre Blocker <londre.blocker@gmail.com>
2017-08-31 18:55:13 -0400

    Add the Boost library files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include <cstdio>
+#include <boost/array.hpp>
+#include <boost/regex.hpp>
+#include <boost/optional.hpp>
 
-int main() {
+#include <string>
+#include <iostream>
+#include <utility>
+#include <memory>
+#include <fstream>
+
+using namespace std;
+//using namespace boost;
+
+auto main(int argc, char *argv[]) -> int {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
